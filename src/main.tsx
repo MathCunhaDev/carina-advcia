a82bdc34945624010b5f6706c25d4f91bb0afd9e
Matheus Cunha <ext.matheus.cunha@zamp.com.br>
2024-04-20 11:25:02 -0300

    gtm config

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
+import TagManager from "react-gtm-module";
+
+const tagManagerArgs = {
+  gtmId: "G-6DQ5X9MC0Y",
+};
+
+TagManager.initialize(tagManagerArgs);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
